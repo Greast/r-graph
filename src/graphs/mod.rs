c91d577c8cfaa5f27a455ff6b5ce5601b6c701c7
Greast <jonas.jonas.srensen11@gmail.com>
2020-03-13 03:22:23 +0100

    Forwarded imports

--- src/graphs/mod.rs
+++ src/graphs/mod.rs
@@ -1,3 +1,7 @@
 pub mod path;
 pub mod random;
 pub mod oriented;
+
+pub use oriented::Oriented;
+pub use path::Path;
+pub use crate::dev::simple::Simple;
--- src/graphs/random/mod.rs
+++ src/graphs/random/mod.rs
@@ -2,3 +2,4 @@ pub mod edge;
 pub mod vertex;
 
 pub use edge::Edge;
+pub use vertex::Vertex;
